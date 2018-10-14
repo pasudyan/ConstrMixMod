9193de36be2b778a2b45fafb4ffb0a302fbd2b18
pasudyan <pasudyan@gmail.com>
2018-10-14 16:57:51 -0600

    update

--- R/run_gibbs.R
+++ R/run_gibbs.R
@@ -14,7 +14,7 @@ if (!require(req_packages)){
 #'  Main function for inference
 #'
 #' This function evaluates the cumulative distribution of a univariate Gaussian distribution
-#' @param mix one of "tmog" and "motg"
+#' @param mix Mixture type: one of "tmog" and "motg"
 #' @param data_type one of "gauss", "beta", "bvgauss", "flowcyto", "crime"
 #' @param thr vector of number of rejections per observations.
 #' @return samples from posterior distribution
@@ -22,18 +22,37 @@ if (!require(req_packages)){
 #'
 Infernce_ConstrMixMod <- function(mix, data_type, thr){
 
+  # Check correct inputs
+  if (!(mix %in% c("tmog", "motg"))){
+    stop("Mixture type must be one of 'tmog' or 'motg'")
+  }
+
+  if (!(data_type %in% c("gauss", "beta", "bvgauss", "flowcyto", "crime"))){
+    stop("Data type must be one of 'gauss', 'beta', 'bvgauss', 'flowcyto', 'crime'")
+  }
+
+  if (thr < 0){
+    stop("Value of thr must be greater than 0")
+  }
+
+  # Sampling method for computation of the normalization function
+  # "is" uses important sampling
+  # package uses existing truncated Gaussian functions
   if (data_type == "crime"){
     samp_method <- "is"
   } else {
     samp_method <- "package"
   }
 
+
   K <- 50
   burnIn   <- 2000
   numIter  <- 5000
+
+  # Maximum number of rejections
   ceil_rej <- 100000
 
-  # Thinning value
+  # Thinning value for the MCMC samples on the posterior
   thin_sc <- 1
   thr_l   <- length(thr)
 
@@ -52,6 +71,7 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
   }
 
   if (dm == 1){
+
     # Bounds for the domain
     lower <- 0
     upper <- 1
@@ -64,12 +84,12 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
     ip_data <- ip_data_all[constr(ip_data_all)]
 
     num_obs <- length(ip_data)
-    # num_obs <- 200
 
     num_obs_sub <- sample.int(length(ip_data_all), num_obs, replace = F)
     ip_data     <- ip_data_all[num_obs_sub]
     ip_data     <- matrix(ip_data, nrow = 1, ncol = num_obs)
 
+    # ********** Use only test data near the borders ********** #
     bound_data <- (ip_data - 0.5)*1.2 + 0.5
     bound_data_cs <- constr(bound_data)
 
@@ -82,9 +102,39 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
     mu_pr <- mean(ip_data)
     sd_pr <- sd(ip_data)
 
-    # Grid points for predictions
+    # Grid points for posterior predictive distribution
     delta   <- 0.005
     pts     <- seq(0, 1, delta)
+  }
+
+  if (data_type == "bvgauss"){
+
+    # Load data
+    load("sim_data/data_bvgauss_1000_2corner_modes.RData")
+
+    # Function to eliminate points outside the hypercube
+    genconst <- function(u1, u2, l1, l2) {
+      in_set <- function(X) {
+        if (nrow(X) == 1) X = t(X)
+        X[1,] > l1 & X[1,] < u1 & X[2,] > l2 & X[2,] < u2
+      }
+    }
+
+    dm <- dim(ip_data_all)[1]
+    l  <- 0
+    u  <- 1
+
+    constr  <- genconst(u, u, l, l)
+
+    num_obs <- ncol(ip_data_all)
+
+    smp <- sample.int(ncol(ip_data_all), num_obs, replace = F)
+    ip_data <- ip_data_all[, smp]
+
+    lower <- rep(l, dm)
+    upper <- rep(u, dm)
+
+    test_ind <- sample.int(num_obs, ceiling(0.2*num_obs), replace = F)
   }
 
   if (data_type == "flowcyto"){
@@ -92,11 +142,9 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
     # Use flowcytometry data
     data(GvHD)
 
-    # Scale of data
+    # Scale data
     sc  <- 1024
-
     ip_data_ctrl <- t(GvHD.control)/sc
-    ip_data_pos  <- t(GvHD.pos)/sc
 
     # Function to eliminate points outside the hypercube
     genconst <- function(u1, u2, l1, l2) {
@@ -108,14 +156,10 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
       }
     }
 
-    l1 <- 0
-    l2 <- 0
-    u  <- 1
+    l1 <- 0; l2 <- 0; u  <- 1
 
     constr  <- genconst(u, u, l1, l2)
-
-    num_obs     <- ncol(ip_data_ctrl) #use all data
-
+    num_obs <- ncol(ip_data_ctrl)
     ip_data <- ip_data_ctrl
 
     dm  <- dim(ip_data)[1]
@@ -123,10 +167,10 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
     lower   <- rep(l1, dm)
     upper   <- rep(u, dm)
 
+    # **************** Use only test data near the borders ***************** #
     bound_data <- apply(ip_data, 2, function(x) min(x) < 0.01 | max(x) > 0.99)
 
     ind_out  <- which(bound_data == TRUE)
-    length(ind_out)
 
     num_test <- 100
     test_ind <- sample(ind_out, num_test, replace = FALSE)
@@ -136,16 +180,14 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
   if (data_type == "crime"){
 
     # Load crime data and constraints for the data
-    # load("./crime_data/chicago_poly.RData") #scaled polygon
     load("./crime_data/chi_murder_2012_2017.RData")
     load("./crime_data/Neigh_coord_list.RData")
 
-    scale_crime <- 5
-
-    #scale data
+    # Scaling data
+    scale_crime   <- 5
     murder_loc_sc <- murder_loc_sc*scale_crime
 
-    #scale polygon
+    # Scale borders of Chicago
     coord_list_sc <- lapply(1:length(coord_list_sc_t), function(x) {
       tmp <- coord_list_sc_t[[x]]
       return(tmp*scale_crime)
@@ -178,78 +220,51 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
       cnstr_chicago(XX, l_poly)
     }
 
+    # **************** Use only test data near the borders ***************** #
     bound_data <- t((t(murder_loc_sc) - c(1, 1.5))*1.2 + c(1, 1.5))
     bound_data_cs <- constr(t(bound_data))
 
+    num_test <- 200
     ind_out  <- which(bound_data_cs == FALSE)
-    test_ind <- sample(ind_out, 200, replace = FALSE)
+    test_ind <- sample(ind_out, num_test, replace = FALSE)
 
     ip_data <- t(murder_loc_sc)
     num_obs <- ncol(ip_data)
 
     dm <- dim(ip_data)[1]
-
   }
 
-  if (data_type == "bvgauss"){
-
-    load("sim_data/data_bvgauss_1000_2corner_modes.RData")
-    # load("sim_data/data_bvgauss_1000_3modes.RData")
-
-    # Function to eliminate points outside the hypercube
-    genconst <- function(u1, u2, l1, l2) {
-      in_set <- function(X) {
-        if (nrow(X) == 1) X = t(X)
-        X[1,] > l1 & X[1,] < u1 & X[2,] > l2 & X[2,] < u2
-      }
-    }
-
-    dm <- dim(ip_data_all)[1]
-    l  <- 0
-    u  <- 1 #upper constraints for 2 modes
-    # u <- 3/2 #upper constraints for 3 modes
-
-    constr  <- genconst(u, u, l, l)
-
-    num_obs <- ncol(ip_data_all)
-    num_obs <- 200
-
-    smp <- sample.int(ncol(ip_data_all), num_obs, replace = F)
-    ip_data <- ip_data_all[, smp]
-
-    lower <- rep(l, dm)
-    upper <- rep(u, dm)
-  }
-
-  # ----------------------- Cross Validation ------------------------ #
+  # ======================= Main Inference ======================= #
 
   # Divide into test and train set with ratio 80:20
-  # sub     <- sample.int(num_obs, ceiling(0.8*num_obs), replace = F)
   sub <- seq(1, num_obs, 1)
   sub <- sub[-test_ind]
 
   train   <- ip_data[, sub]
-  test    <- matrix(ip_data[, test_ind], nrow = 2)
+  test    <- matrix(ip_data[, test_ind], nrow = dm)
 
+  # Convert data to matrix
   if (dm == 1){
     train <- matrix(train, nrow = 1, ncol = length(train))
     test  <- matrix(test, nrow = 1, ncol = length(test))
   }
 
+  # Initialize variables
   time.taken    <- c()
   test_pred     <- list()
   prod_pred_ts  <- c()
   prod_pred_pt  <- c()
   count_bins    <- list()
-
   rslt <- list()
 
   print(paste("Mix:", mix))
   print(paste("Data Type:", data_type))
 
+  # Loop over all thresholds
   for (j in 1:length(thr)){
     print(paste("Threshold: ", thr[j]))
 
+    # Function to run Gibbs sampling and timed
     start.time <- system.time({
       rslt[[j]] <- inf_gvhd(K, ip_data = train, axs,
                             thr = thr[j], burnIn, numIter,
@@ -258,31 +273,28 @@ Infernce_ConstrMixMod <- function(mix, data_type, thr){
 
     time.taken[j] <- start.time[3]
 
-
-    if (type == 1){
-      if (mix == "motgt" | mix == "motg"){
+    # Functions to compute posterior predictive probabilities of the test set
+    if (dm == 1){
+      if (mix == "motg")
         test_pred[[j]]  <- post_pred_motgt(res = rslt[[j]]$res,
                                            test, K, pts, upper, lower,
                                            data_type, pr1, pr2)
-      }
-
-      if (mix == "tmogt"){
+      if (mix == "tmogt")
         test_pred[[j]]  <- post_pred_tmogt(res = rslt[[j]]$res,
-                                           test, K, pts, upper, lower, data_type, pr1, pr2)
-      }
+                                           test, K, pts, upper, lower,
+                                           data_type, pr1, pr2)
 
       prod_pred_ts[j]    <- sum(log(test_pred[[j]]$pred_ts))
       prod_pred_pt[j]    <- sum(log(test_pred[[j]]$pred_pt))
 
     } else {
-      if (mix == "motgt" | mix == "motg"){
+      if (mix == "motg")
         test_pred[[j]]  <- post_pred_motgt_mv(res = rslt[[j]]$res,
                                               test, K, lower, upper, constr)
-      }
 
-      if (mix == "tmogt"){
-        test_pred[[j]]  <- post_pred_tmogt_mv(res = rslt[[j]]$res, test, K, constr, thin_sc)
-      }
+      if (mix == "tmogt")
+        test_pred[[j]]  <- post_pred_tmogt_mv(res = rslt[[j]]$res, test, K,
+                                              constr, thin_sc)
 
       prod_pred_ts[j]    <- sum(log(test_pred[[j]]$pred_ts))
     }
